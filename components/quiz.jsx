b447b69d46e1a7227aceaf9f53e801e9f32617e0
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-04-23 20:24:44 +0500

    answers are now shuffled, app now doesnot crash if quiz is completed

--- components/quiz.jsx
+++ components/quiz.jsx
@@ -1,4 +1,5 @@
 import { act, useState } from "react";
+import quizCompleteImg from "../src/assets/quiz-complete.png";
 
 import QUESTION from "../src/questions";
 
@@ -6,6 +7,19 @@ export default function Quiz() {
   const [selectedAnswer, setSelectedAnswer] = useState([]);
 
   const activeQuestionIndex = selectedAnswer.length;
+  const quizComplete = selectedAnswer.length === QUESTION.length;
+
+  if (quizComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="trophy image" />
+        <h2>quiz completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = QUESTION[activeQuestionIndex].answers;
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   function handleSelectedAnswer(selectedAnswer) {
     setSelectedAnswer((previousAnswers) => {
@@ -18,7 +32,7 @@ export default function Quiz() {
       <div id="questions">
         <h2>{QUESTION[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTION[activeQuestionIndex].answers.map((answer) => {
+          {shuffledAnswers.map((answer) => {
             return (
               <li key={answer} className="answer">
                 <button onClick={() => handleSelectedAnswer(answer)}>
